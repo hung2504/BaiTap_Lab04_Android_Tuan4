3f68a5ef6ef33e8040b506aaf05050ad2f3dfc01
hung2504 <vanhung25042000@gmail.com>
2021-05-16 16:08:58 +0700

    Initial commit

---
+++ app/src/main/java/com/example/baitap_lab04_android_tuan4/BaiTap_4a.java
@@ -0,0 +1,14 @@
+package com.example.baitap_lab04_android_tuan4;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class BaiTap_4a extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
